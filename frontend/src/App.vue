fe56024bd336303db57e861bc252b18749ff1775
Joshua-Harmon <joshua.harmon1960@gmail.com>
2022-07-10 12:10:38 -0700

    test

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <dropzone></dropzone>
+  </div>
+</template>
+
+<script>
+import Dropzone from './components/Dropzone'
+
+export default {
+  name: 'app',
+  components: {
+    Dropzone
+  }
+}
+</script>
+
+<style lang="stylus">
+body
+  font-family 'Open Sans', sans-serif
+</style>
